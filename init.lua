fcd8346883f69c4777565709300c1c50b87a42eb
AaronBerndt <aaron.berndt2@gmail.com>
2024-03-01 06:56:01 -0600

    Updated lua config

--- init.lua
+++ init.lua
@@ -14,34 +14,30 @@ vim.g.mapleader = " "
 
 require("lazy").setup({
 "folke/which-key.nvim",
+"folke/neodev.nvim",
+"shaunsingh/nord.nvim",
+'github/copilot.vim',
+'metakirby5/codi.vim',
+'svermeulen/vim-subversive',
 { 
     "folke/neoconf.nvim", 
     cmd = "Neoconf" 
 },
-"folke/neodev.nvim",
-"shaunsingh/nord.nvim",
-'github/copilot.vim',
-{    "tpope/vim-fugitive",
-      config = function()
-	vim.keymap.set('n', '<leader>gs', ':Git<CR>', {})
-	vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', {})
-	vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {})
-	vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', {})
-	vim.keymap.set('n', '<leader>gl', ':Glog<CR>', {})
-	vim.keymap.set('n', '<leader>gp', ':Git push<CR>', {})
-	vim.keymap.set('n', '<leader>gr', ':Gread<CR>', {})
-	vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', {})
-	vim.keymap.set('n', '<leader>gj', ':diffget //3<CR>', {})
-	vim.keymap.set('n', '<leader>gk', ':diffget //2<CR>', {})
-      end
 
+{    
+    "tpope/vim-fugitive",
+     config = function()
+       vim.keymap.set('n', '<leader>gs', ':Git<CR>', {})
+       vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', {})
+       vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {})
+       vim.keymap.set('n', '<leader>gp', ':Git push<CR>', {})
+     end
 },
-'metakirby5/codi.vim',
-'svermeulen/vim-subversive',
 {
     'akinsho/toggleterm.nvim', 
     version = "*", 
     config = true
+    
 },
 {
     "kylechui/nvim-surround",
@@ -77,62 +73,62 @@ require("lazy").setup({
      dependencies = { 'nvim-tree/nvim-web-devicons' },
      config = function()
 
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
+       local colors = {
+         blue   = '#80a0ff',
+         cyan   = '#79dac8',
+         black  = '#080808',
+         white  = '#c6c6c6',
+         red    = '#ff5189',
+         violet = '#d183e8',
+         grey   = '#303030',
+       }
 
-local bubbles_theme = {
-   normal = {
-      a = { fg = colors.black, bg = colors.violet },
-      b = { fg = colors.white, bg = colors.grey },
-      c = { fg = colors.black, bg = colors.black },
-   },
-   insert = { a = { fg = colors.black, bg = colors.blue } },
-   visual = { a = { fg = colors.black, bg = colors.cyan } },
-   replace = { a = { fg = colors.black, bg = colors.red } },
+       local bubbles_theme = {
+          normal = {
+            a = { fg = colors.black, bg = colors.violet },
+            b = { fg = colors.white, bg = colors.grey },
+            c = { fg = colors.black, bg = colors.black },
+         },
+         insert = { a = { fg = colors.black, bg = colors.blue } },
+         visual = { a = { fg = colors.black, bg = colors.cyan } },
+         replace = { a = { fg = colors.black, bg = colors.red } },
 
-   inactive = {
-     a = { fg = colors.white, bg = colors.black },
-     b = { fg = colors.white, bg = colors.black },
-     c = { fg = colors.black, bg = colors.black },
-   },
- }
- require('lualine').setup({
-   options = {
-     theme = bubbles_theme,
-      component_separators = '|',
-      section_separators = { left = '', right = '' },
-    },
-    sections = {
-      lualine_a = {
-        { 'mode', separator = { left = '' }, right_padding = 2 },
-      },
-      lualine_b = { 'filename', 'branch' },
-      lualine_c = { 'fileformat' },
-      lualine_x = {}  ,
-      lualine_y = { 'filetype', 'progress' },
-      lualine_z = {
-        { 'location', separator = { right = '' }, left_padding = 2 },
-      },
-    },
-    inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-    },
-    tabline = {},
-    extensions = {}
-  })
-  end
+         inactive = {
+           a = { fg = colors.white, bg = colors.black },
+           b = { fg = colors.white, bg = colors.black },
+           c = { fg = colors.black, bg = colors.black },
+         },
+       }
+       require('lualine').setup({
+         options = {
+           theme = bubbles_theme,
+            component_separators = '|',
+            section_separators = { left = '', right = '' },
+          },
+          sections = {
+            lualine_a = {
+              { 'mode', separator = { left = '' }, right_padding = 2 },
+            },
+            lualine_b = { 'filename', 'branch' },
+            lualine_c = { 'fileformat' },
+            lualine_x = {}  ,
+            lualine_y = { 'filetype', 'progress' },
+            lualine_z = {
+              { 'location', separator = { right = '' }, left_padding = 2 },
+            },
+          },
+          inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+	    lualine_c = {},
+	    lualine_x = {},
+	    lualine_y = {},
+	    lualine_z = { 'location' },
+	    },
+	    tabline = {},
+	    extensions = {}
+	  })
+      end
 },
 {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
@@ -209,6 +205,7 @@ local bubbles_theme = {
     "folke/trouble.nvim",
      dependencies = {"nvim-tree/nvim-web-devicons"},
      opts = {}
+
 },
 {
   "folke/tokyonight.nvim",
@@ -222,7 +219,9 @@ local bubbles_theme = {
 vim.cmd[[colorscheme tokyonight-storm]]
 vim.opt.number = true
 vim.opt.termguicolors = true
+vim.opt.cursorline = true
 
 -- keybindings
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "yy", ":TerminalToggle direction='float' <CR>")
