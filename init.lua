bbb18e759fafeded4e2623181515a6a3eaa78d8a
AaronBerndt <aaron.berndt2@gmail.com>
2024-03-07 20:28:12 -0600

    Removed used stuff

--- init.lua
+++ init.lua
@@ -36,11 +36,10 @@ require("lazy").setup({
 		"stevearc/conform.nvim",
 		opts = {},
 
-		config = function(args)
+		config = function()
 			require("conform").setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
-					-- Conform will run multiple formatters sequentially
 					python = { "isort", "black" },
 					typescript = { { "prettierd" } },
 					typescriptreact = { { "prettierd" } },
