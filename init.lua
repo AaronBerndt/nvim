53cfaa0cf2f9e7553ef02d1cde2708180cae70be
AaronBerndt <aaron.berndt2@gmail.com>
2024-03-20 18:58:31 -0500

    Added autocomplete

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
-
 require("lazy").setup({
 	"folke/which-key.nvim",
 	"folke/neodev.nvim",
@@ -269,6 +268,45 @@ require("lazy").setup({
 				},
 			},
 		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = {
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "cmdline" },
+					{ name = "luasnip" },
+				},
+			})
+		end,
 	},
 })
 
