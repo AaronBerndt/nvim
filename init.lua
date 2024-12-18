832404ba3d33d89f6204a2c0ff08c3df6893b8dd
AaronBerndt <aaron.berndt2@gmail.com>
2024-12-18 06:52:41 -0600

    Added harpoon

--- init.lua
+++ init.lua
@@ -21,6 +21,11 @@ require("lazy").setup({
 	"metakirby5/codi.vim",
 	"svermeulen/vim-subversive",
 	{ "glacambre/firenvim", build = ":call firenvim#install(0)" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 	{ "mistweaverco/kulala.nvim", opts = {} },
 	{
 		"folke/neoconf.nvim",
@@ -196,10 +201,61 @@ require("lazy").setup({
 
 		config = function()
 			local builtin = require("telescope.builtin")
+			local conf = require("telescope.config").values
+			local harpoon = require("harpoon")
+			harpoon:setup({})
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			vim.keymap.set("n", "<leader>fh", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "Open harpoon window" })
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			-- vim.keymap.set("n", "<C-e>", function()
+			-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
+			-- end)
+
+			vim.keymap.set("n", "<C-h>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-t>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-n>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-s>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 		end,
 	},
@@ -254,7 +310,7 @@ require("lazy").setup({
 			local lsp_zero = require("lsp-zero")
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "ts_ls", "lua_ls", "eslint", "html","solargraph","coffeesense","angularls" },
+				ensure_installed = { "ts_ls", "lua_ls", "eslint", "html", "solargraph", "coffeesense", "angularls" },
 			})
 
 			on_attach = function(_, _)
